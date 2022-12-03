d5444039e13a9711683716f85d02c06faa105d20
Glitch (glitch-hello-website) <none>
2022-12-03 10:48:59 +0000

    📷🛰 Checkpoint ./LICENSE:33983566/2707 ./README.md:33983566/2133 ./TODO.md:33983566/779 ./script.js:33983566/643 ./style.css:33983566/1243

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 /*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
+SAOECOM
 */
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+// SAOECOM
+// SAOECOM
 console.log("Hello 🌎");
 
 /* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
+SAOECOM
 */
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
+const btn = document.querySelector("button"); // SAOECOM
+// SAOECOM
 if (btn) {
   btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
+    // TSAOECOM
     btn.classList.toggle("dipped");
   };
 }
